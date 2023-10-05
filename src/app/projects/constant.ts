8d8b53be67441e67d6aea4a36c833387df589d99
Parbat <parbat@pop-os.localdomain>
2023-10-05 22:19:52 +0545

    Feat: Integrate infinite scrolling in problm sync table

---
+++ src/app/projects/constant.ts
@@ -0,0 +1,9 @@
+export const pageStyles = {
+    wrapper: 'wrapper',
+    jobWrapper: 'sm:w-full',
+    title: 'mt-9 mb-8 animateOff',
+    divider: 'divider',
+    feed_child1: 'flex-between',
+    feed_child1_searchIcon: "text-lg cursor-pointer",
+    feed_child2: 'my-5 flex flex-wrap justify-center sm:justify-between '
+}
