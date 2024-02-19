7e809a57166d0c90dc9610c0cd8095b9fa61f261
Parbat Lama <lamaparbat70@gmail.com>
2024-02-19 21:24:12 +0545

    Fix: Entire homepage text color adjustment

--- src/Components/AcademicCard/page.tsx
+++ src/Components/AcademicCard/page.tsx
@@ -13,7 +13,7 @@ const AcademicCard = ({
 
     return (
         <div className='sm:ml-[60px] academic_card animateOff w-full flex-between mb-5 sm:shadow-lg dark:bg-zinc-900 dark:shadow-black'>
-            <div className='h-[70px] w-[100px] flex justify-center items-center  rounded border-r-2 border-green-500 sm:flex overflow-hidden bg-white'>
+            <div className='h-[70px] w-[100px] hidden justify-center items-center rounded border-r-2 border-green-500 sm:flex overflow-hidden bg-white'>
                 {typeof logo === 'string' ?
                     <img
                         src={logo}
